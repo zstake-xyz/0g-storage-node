a4d0f7ac203d8e85dda68a1049e1e7203e2b85d9
Zstake <hyunjubd@gmail.com>
2024-07-31 03:19:51 +0900

    Delete common directory

--- common/contract-interface/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if cfg!(not(feature = "dev")) {
-        println!("cargo:rerun-if-changed=../../0g-storage-contracts/artifacts/");
-    } else {
-        println!("cargo:rerun-if-changed=../../0g-storage-contracts-dev/artifacts/");
-    }
-}
